88d6a4e2733be31ea84aa279eae4dfaf286a3517
kazantsev-roman <dj.ka3anova@gmail.com>
2023-01-17 19:43:36 +0300

    добавил и реализовал функцию получения данных из файла

--- src/Infrastructure/ModelProvider.cpp
+++ src/Infrastructure/ModelProvider.cpp
@@ -1,6 +1,36 @@
+#include <fstream>
+#include <sstream>
 #include "ModelProvider.h"
 
 Model ModelProvider::GetModel(std::string const& filename)
 {
 	return {};
+}
+
+ModelProvider::Spreadsheet ModelProvider::GetDataFromFile(std::string const& filename)
+{
+	std::ifstream input(filename);
+	if (!input.is_open())
+	{
+		throw std::runtime_error("failed to open input file for reading");
+	}
+
+	Spreadsheet spreadsheet;
+	std::string row;
+	std::string cell;
+
+	while (std::getline(input, row))
+	{
+		std::istringstream iss(row);
+		std::vector<std::string> rows;
+
+		while (std::getline(iss, cell, CSV_SEPARATOR))
+		{
+			rows.push_back(cell);
+		}
+
+		spreadsheet.push_back(rows);
+	}
+
+	return spreadsheet;
 }
--- src/Infrastructure/ModelProvider.h
+++ src/Infrastructure/ModelProvider.h
@@ -6,4 +6,11 @@ class ModelProvider : public IModelProvider
 {
 public:
 	Model GetModel(std::string const& filename) override;
+
+private:
+	using Spreadsheet = std::vector<std::vector<std::string>>;
+
+	static auto const CSV_SEPARATOR = ' ';
+
+	static Spreadsheet GetDataFromFile(std::string const& filename);
 };
